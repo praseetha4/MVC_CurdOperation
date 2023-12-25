10f76a42d2438a967373a179da26a880d8e0d899
Praseetha <User@PRASEETHA>
2023-12-25 20:35:30 +0530

    Added Validations

--- MVC_CurdOperation/Models/Employee.cs
+++ MVC_CurdOperation/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -10,27 +11,44 @@ namespace MVC_CurdOperation.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required]
+        [DisplayName("Name")]
+        [Required(ErrorMessage = "Name is required.")]
         public string Name { get; set; }
-        [Required]
+
+        [DisplayName("Date Of Birth")]
+        [Required(ErrorMessage = "Date of Birth is required.")]
+        [DataType(DataType.Date, ErrorMessage = "Invalid Date of Birth.")]
         public string DateOfBirth { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Address is required.")]
         public string Address { get; set; }
-        [Required]
+
+        [DisplayName("Mobile No")]
+        [Required(ErrorMessage = "Mobile Number Required")]
+        [Phone(ErrorMessage = "Invalid Mobile No")]
         public string MobileNo { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Gender is required.")]
         public string Gender { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Age is required.")]
+        [Range(18, 99, ErrorMessage = "Age must be between 18 and 60.")]
         public int Age { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Position is required.")]
         public string position { get; set; }
-        [Required]
+
+        
+        [DisplayName("Date Of Hire")]
+        [Required(ErrorMessage = "Date of Hire is required.")]
+        [DataType(DataType.Date, ErrorMessage = "Invalid Date of Hire.")]
         public string DateOfHire { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Department is required.")]
         public string Department { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Salary is required.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Salary must be greater than 0.")]
         public double Salary { get; set; }
     }
 }
